a8179d00135e5424330c92ecae5ea963e2a4d65d
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-05-12 09:06:21 +0530

    issue defined

--- pydantic_part.py
+++ pydantic_part.py
@@ -7,7 +7,15 @@ def home():
     return "API is Made with 💖"
 
 # what issue looks like 
-
+def insert_patient_data(name:str,age:int):
+    """
+    This function will accept age as a string also this means no type checking and validation
+    """
+    print(name)
+    print(age)
+    print("Insrted sUCcessfully")
+    
+insert_patient_data("feroz","thiry two")
 
 # how we can solve issue 
 
