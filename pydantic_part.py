05f4aa461acbc054cb38ffb66c0a9a1283399105
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-05-12 09:09:47 +0530

    how we solve the issue

--- pydantic_part.py
+++ pydantic_part.py
@@ -14,10 +14,17 @@ def insert_patient_data(name:str,age:int):
     print(name)
     print(age)
     print("Insrted sUCcessfully")
-    
+
 insert_patient_data("feroz","thiry two")
 
 # how we can solve issue 
+def insert_name_and_age(name:str,age:int):
+    if type(name)==str and type(age) == int:
+        print(name)
+        print(age)
+        print("inserted succesfully ")
+    else:
+        raise TypeError("Incorrect datatype")
 
-
+insert_name_and_age("fairoz",32)
 # how pydantic solves it
