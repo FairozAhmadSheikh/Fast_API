eeeb9cadc68fac28096d9b42d9c34e751af191f6
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-05-16 15:59:34 +0530

    Query parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Path , HTTPException
+from fastapi import FastAPI , Path , HTTPException,Query
 import json
 """
     The app would be designed something like a place where a doctor stores his patient data
@@ -52,3 +52,19 @@ def get_pateint(patient_id:str=Path(...,description="ID Of the patient in the Da
         return data[patient_id]
     else:
         raise HTTPException(status_code=404,detail="Patient not found")
+
+# Route with querry parameter
+
+@app.get('/sort')
+def sort_patients(sort_by:str= Query(...,description="Sort on the basis of height weight bmi "),order:str=Query(...,description='Sort in asc and desc order')):
+    valid_feilds=['height','weight','bmi']
+    if sort_by not in valid_feilds:
+        raise HTTPException(status_code=400,detail=f'Invalid feild selected from {valid_feilds}')
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=404,detail=f'Invalid order selected from asc and desc')
+    
+    data=load_data()
+    sort_order=True if order=='desc' else False
+
+    sort_order=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sort_order
